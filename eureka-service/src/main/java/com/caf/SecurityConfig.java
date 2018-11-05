f910dd355a762657290dcccbd3927c18070cd4df
程声强 <chengshengqiang@jsh.com>
2018-11-05 21:43:48 +0800

    eureka-service 集群配置及设置权限

---
+++ eureka-service/src/main/java/com/caf/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.caf;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration
+    .WebSecurityConfigurerAdapter;
+
+/**
+ * Created by caf on 2018/11/5.
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+    http.csrf().disable();
+  }
+}
